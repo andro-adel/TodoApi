16a3b7cf3432bf80ec1d2b634c4fbe8cfa6be935
Andro Adel <a.Adel.c@ncnp.gov.sa>
2025-07-22 23:39:29 +0300

    finish project

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
